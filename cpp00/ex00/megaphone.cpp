bfaf31b032f79ca14b0f96f65cd530faf01f5d5a
Marwan <111853245+kurapikap@users.noreply.github.com>
2023-12-06 15:48:42 +0100

    update

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -3,17 +3,17 @@
 
 int main(int ac, char **av)
 {
-    if (ac == 1)
+    if(ac == 1)
         std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
     else
     {
-        for (int i = 1; i < ac; i++)
+        for(int i = 1; i < ac; i++)
         {
-            for (int j = 0; av[i][j]; j++)
+            for(int j = 0; av[i][j]; j++)
                 std::cout << (char)std::toupper(av[i][j]);
             std::cout << " ";
         }
     }
     std::cout << std::endl;
-    return (0);
+    return 0;
 }
