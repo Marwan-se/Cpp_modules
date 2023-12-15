df37d75fa81fd9323ea7137eb91378c55273a190
kurapikap <marwansekh@gmail.com>
2023-12-15 23:21:40 +0100

    ex00

---
+++ cpp01/ex00/Zombie.cpp
@@ -0,0 +1,31 @@
+#include "Zombie.hpp"
+
+void    Zombie::announce(void)
+{
+    std::cout  << name << " BraiiiiiiinnnzzzZ..." << std::endl;
+}
+
+std::string Zombie::getName(void)
+{
+    return (name);
+}
+
+void Zombie::setName(std::string name)
+{
+    this->name = name;
+}
+
+Zombie::Zombie()
+{
+}
+
+Zombie::Zombie(std::string name)
+{
+    this->name = name;
+    std::cout << "Zombie created" << std::endl;
+}
+
+Zombie::~Zombie()
+{
+    std::cout << "Zombie destroyed" << std::endl;
+}
