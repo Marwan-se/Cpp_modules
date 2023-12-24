d0338f4fa413df3457ef685aeec1e2c8a528fc61
Marwan Sekhsoukh <msekhsou@e2r4p2.1337.ma>
2023-12-24 02:15:09 +0100

    all done

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -11,9 +11,43 @@
 /* ************************************************************************** */
 
 #include <iostream>
-
-#include <iostream>
 #include <fstream>
-#include <stdio.h>
-#include <string>
 
+int main(int ac, char **av)
+{
+	if (ac != 4)
+	{
+		std::cout << "Error: wrong number of arguments" << std::endl;
+		return (1);
+	}
+	std::string filename = av[1];
+	std::string s1 = av[2];
+	std::string s2 = av[3];
+	std::ifstream ifs(filename);
+	if (!ifs.is_open())
+	{
+		std::cout << "Error: file does not exist" << std::endl;
+		return (1);
+	}
+	std::string content;
+	std::string line;
+	while (std::getline(ifs, line))
+	{
+		if (!ifs.eof())
+			content += line + '\n';
+		else
+			content += line;
+	}
+	ifs.close();
+	size_t pos = 0;
+	while ((pos = content.find(s1, pos)) != std::string::npos)
+	{
+		content.erase(pos, s1.length());
+		content.insert(pos, s2);
+		pos += s2.length();
+	}
+	std::ofstream ofs(filename + ".replace");
+	ofs << content;
+	ofs.close();
+	return (0);
+}
