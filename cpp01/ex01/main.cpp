cb77be6ad92c017901e83eac638ab33bc803a27f
Marwan Sekhsoukh <msekhsou@e3r1p16.1337.ma>
2023-12-25 10:32:20 +0100

    ex01

--- cpp01/ex01/main.cpp
+++ cpp01/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: msekhsou <msekhsou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/21 15:10:39 by msekhsou          #+#    #+#             */
-/*   Updated: 2023/12/21 15:40:06 by msekhsou         ###   ########.fr       */
+/*   Updated: 2023/12/25 10:31:50 by msekhsou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int main(void)
 {
     int i = 0;
     int N = 5;
-    Zombie *zombies = zombieHorde(N, "yes");
+    Zombie *zombies = zombieHorde(N, "Zombie10");
     while (i < N)
     {
         zombies[i].announce();
