5fb96de1655b2f686423a26e3ea19b39f82e7ac4
Marwan Sekhsoukh <msekhsou@e2r2p2.1337.ma>
2024-02-05 14:20:25 +0100

    ex01

--- cpp02/ex01/Fixed.hpp
+++ cpp02/ex01/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: msekhsou <msekhsou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/03 18:07:13 by msekhsou          #+#    #+#             */
-/*   Updated: 2024/02/04 16:46:26 by msekhsou         ###   ########.fr       */
+/*   Updated: 2024/02/04 23:32:31 by msekhsou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,18 +24,27 @@ class   Fixed
         int store_fixed_point;
         static const int bits_num;
     public:
+        //default constructor
         Fixed();
+        //int constructor
         Fixed(const int value);
+        //float constructor
         Fixed(const float value);
+        //copy constructor
         Fixed(const Fixed &value);
+        //assignment operator
 		Fixed &operator=(const Fixed &value);
+        //destructor
         ~Fixed();
+        //getters and setters
         int getRawBits( void ) const;
         void setRawBits( int const raw );
+        //converters
         float toFloat( void ) const;
         int toInt( void ) const;
 };
 
+//overload << operator
 std::ostream &operator<<(std::ostream &out, const Fixed &value);
 
 #endif
