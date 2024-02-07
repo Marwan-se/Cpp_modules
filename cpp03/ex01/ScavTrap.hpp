b608a07dc73fa0bbed31b6fd7787d09afed26311
Marwan Sekhsoukh <marwansekh123@gmail.com>
2024-02-07 18:12:36 +0100

    ex01

--- cpp03/ex01/ScavTrap.hpp
+++ cpp03/ex01/ScavTrap.hpp
@@ -6,7 +6,29 @@
 /*   By: msekhsou <msekhsou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/07 16:50:46 by msekhsou          #+#    #+#             */
-/*   Updated: 2024/02/07 16:50:47 by msekhsou         ###   ########.fr       */
+/*   Updated: 2024/02/07 18:12:25 by msekhsou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef SCAVTRAP_HPP
+# define SCAVTRAP_HPP
+
+#include "ClapTrap.hpp"
+#include <iostream>
+#include <string>
+
+class   ScavTrap : public ClapTrap
+{
+    public:
+        ScavTrap();
+        ScavTrap(std::string name);
+        ScavTrap(const ScavTrap &copy);
+        ~ScavTrap();
+
+        ScavTrap&   operator=(const ScavTrap &copy);
+
+        void        challengeNewcomer(std::string const & target);
+};
+
+
+#endif
