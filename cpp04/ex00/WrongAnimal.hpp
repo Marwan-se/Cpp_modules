7aa37a912a25b464b8e04d53513d7c4285cc356c
Marwan Sekhsoukh <marwansekh123@gmail.com>
2024-02-09 22:12:08 +0100

    ex00

--- cpp04/ex00/WrongAnimal.hpp
+++ cpp04/ex00/WrongAnimal.hpp
@@ -6,7 +6,7 @@
 /*   By: msekhsou <msekhsou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/08 21:07:13 by msekhsou          #+#    #+#             */
-/*   Updated: 2024/02/08 21:11:55 by msekhsou         ###   ########.fr       */
+/*   Updated: 2024/02/09 22:11:52 by msekhsou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,12 +28,12 @@ class WrongAnimal
         //assignment operator
         WrongAnimal &operator=(const WrongAnimal &rhs);
         //destructor
-        ~WrongAnimal();
+        virtual ~WrongAnimal();
         //getters
-        std::string getType() const;
+        std::string getType();
         //setters
         void setType(std::string value);
-        //member function
+        //member functions
         void makeSound() const;
 };
 
