f237b69a537c22f4c6aa818d36622156616aba4c
Marwan Sekhsoukh <marwansekh123@gmail.com>
2024-02-09 22:12:28 +0100

    ex00

--- cpp04/ex00/WrongAnimal.cpp
+++ cpp04/ex00/WrongAnimal.cpp
@@ -6,8 +6,14 @@
 /*   By: msekhsou <msekhsou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/09 22:12:03 by msekhsou          #+#    #+#             */
-/*   Updated: 2024/02/09 22:12:15 by msekhsou         ###   ########.fr       */
+/*   Updated: 2024/02/09 22:12:25 by msekhsou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "WrongAnimal.hpp"
+
+WrongAnimal::WrongAnimal()
+{
+    std::cout << "WrongAnimal default constructor called" << std::endl;
+    this->type = "WRONG ANIMALS";
+}
