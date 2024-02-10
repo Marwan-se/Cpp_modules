31a0709c95d25e400cd7d697fb8e810afa988079
Marwan Sekhsoukh <marwansekh123@gmail.com>
2024-02-10 16:56:21 +0100

    ex01

--- cpp04/ex01/Dog.hpp
+++ cpp04/ex01/Dog.hpp
@@ -6,7 +6,7 @@
 /*   By: msekhsou <msekhsou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/08 19:14:56 by msekhsou          #+#    #+#             */
-/*   Updated: 2024/02/10 15:46:58 by msekhsou         ###   ########.fr       */
+/*   Updated: 2024/02/10 16:56:14 by msekhsou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,22 +21,26 @@ class   Dog : public Animal
     private:
         Brain *brain;
     public:
+
+    /*---------------------- Orthodox Canonical Form ----------------------*/
         //default constructor
         Dog();
-        //parameterized constructor
-        Dog(std::string value);
         //copy constructor
         Dog(const Dog &copy);
         //assignment operator
         Dog &operator=(const Dog &rhs);
         //destructor
         virtual ~Dog();
+    /*---------------------------------------------------------------------*/
+
+    /*----------------------------- Member Functions -----------------------*/
         //getters
         std::string getType();
         //setters
         void setType(std::string value);
         //member functions
         void makeSound() const;
+    /*---------------------------------------------------------------------*/
 };
 
 #endif
