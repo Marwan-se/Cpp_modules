8006866c164944b511b45231319fb81cd725b094
Marwan Sekhsoukh <marwansekh123@gmail.com>
2024-02-10 18:15:40 +0100

    ex01

--- cpp04/ex01/main.cpp
+++ cpp04/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: msekhsou <msekhsou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/08 21:03:55 by msekhsou          #+#    #+#             */
-/*   Updated: 2024/02/10 17:31:26 by msekhsou         ###   ########.fr       */
+/*   Updated: 2024/02/10 18:05:40 by msekhsou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,39 +14,33 @@
 #include "Dog.hpp"
 #include "Cat.hpp"
 
-void vv () {
-    system ("leaks Animals");
-}
-
 int main()
 {
-    
-    Dog a;
-    Dog b(a);
+    Animal *a = new Dog();
+    Animal *b = new Dog();
 
+    *a = *b;
+    delete a;
+    delete b;
 
-    atexit (vv);
-    // b.printIdeas();
+    Dog dog;
+    Cat cat;
 
-    // Dog dog;
-    // Cat cat;
-
-    // const Animal *animals[10];
-    // int i = 0;
-    // while (i < 10)
-    // {
-    //     if (i % 2 == 0)
-    //         animals[i] = new Dog();
-    //     else
-    //         animals[i] = new Cat();
-    //     i++;
-    // }
-    // std::cout << std::endl;
-    // i = 0;
-    // while (i < 10)
-    // {
-    //     delete animals[i];
-    //     i++;
-    // }
+    const Animal *animals[10];
+    int i = 0;
+    while (i < 10)
+    {
+        if (i % 2 == 0)
+            animals[i] = new Dog();
+        else
+            animals[i] = new Cat();
+        i++;
+    }
+    i = 0;
+    while (i < 10)
+    {
+        delete animals[i];
+        i++;
+    }
     return 0;
 }
