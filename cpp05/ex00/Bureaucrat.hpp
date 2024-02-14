eae08bb1ade37a89324dd3fce22d7b5f0e4c0dac
Marwan Sekhsoukh <marwansekh123@gmail.com>
2024-02-14 20:01:47 +0100

    ex00

--- cpp05/ex00/Bureaucrat.hpp
+++ cpp05/ex00/Bureaucrat.hpp
@@ -6,7 +6,36 @@
 /*   By: msekhsou <msekhsou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/12 18:37:15 by msekhsou          #+#    #+#             */
-/*   Updated: 2024/02/12 18:37:16 by msekhsou         ###   ########.fr       */
+/*   Updated: 2024/02/13 17:55:04 by msekhsou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+# include <iostream>
+# include <string>
+#include <exception>
+
+class Bureaucrat
+{
+    private:
+        std::string const name;
+        int grade;
+    public:
+    /*---------------------- Orthodox Canonical Form ----------------------*/
+        Bureaucrat();
+        Bureaucrat(std::string const &name, int grade);
+        Bureaucrat(Bureaucrat const &src);
+        ~Bureaucrat();
+        Bureaucrat &operator=(Bureaucrat const &src);
+    /*---------------------------------------------------------------------*/
+
+    /*---------------------- Member Function ----------------------*/
+        std::string const &getName() const;
+        int getGrade() const;
+        void incrementGrade();
+        void decrementGrade();
+};
+
+#endif
