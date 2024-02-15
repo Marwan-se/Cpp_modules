1392597fc02c858ee321c5869e95f5e012f9218f
kurapikap@example.com <marwansekh123@gmail.com>
2024-02-15 21:54:35 +0000

    ex00

--- cpp05/ex00/Bureaucrat.cpp
+++ cpp05/ex00/Bureaucrat.cpp
@@ -6,7 +6,7 @@
 /*   By: msekhsou <msekhsou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/12 18:36:59 by msekhsou          #+#    #+#             */
-/*   Updated: 2024/02/15 21:49:38 by msekhsou         ###   ########.fr       */
+/*   Updated: 2024/02/15 21:54:34 by msekhsou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,16 @@
 Bureaucrat::Bureaucrat()
 {
     std::cout << "Bureaucrat default constructor called" << std::endl;
-    
+    name = "Default";
+    grade = 150;
+}
+
+Bureaucrat::Bureaucrat(std::string const &name, int grade)
+{
+    std::cout << "Bureaucrat constructor called" << std::endl;
+    if (grade < 1)
+        throw Bureaucrat::GradeTooHighException();
+    else if (grade > 150)
+        throw Bureaucrat::GradeTooLowException();
+    this->grade = grade;
 }
