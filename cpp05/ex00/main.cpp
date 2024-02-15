c71a7f31cb453ef6e33aa7219b2151ff103ede83
kurapikap@example.com <marwansekh123@gmail.com>
2024-02-15 21:16:41 +0000

    ex00

--- cpp05/ex00/main.cpp
+++ cpp05/ex00/main.cpp
@@ -6,8 +6,55 @@
 /*   By: msekhsou <msekhsou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/12 18:34:33 by msekhsou          #+#    #+#             */
-/*   Updated: 2024/02/14 22:47:02 by msekhsou         ###   ########.fr       */
+/*   Updated: 2024/02/15 21:16:36 by msekhsou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
+
+int main()
+{
+    Bureaucrat b1("B1", 1);
+    Bureaucrat b2("B2", 150);
+    Bureaucrat b3("B3", 151);
+    Bureaucrat b4("B4", 0);
+    Bureaucrat b5("B5", 2);
+
+    std::cout << b1 << std::endl;
+    std::cout << b2 << std::endl;
+    std::cout << b3 << std::endl;
+    std::cout << b4 << std::endl;
+    std::cout << b5 << std::endl;
+
+    try
+    {
+        Bureaucrat b6("B6", 152);
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+
+    try
+    {
+        Bureaucrat b7("B7", -1);
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+
+    b1.incrementGrade();
+    b2.decrementGrade();
+    b3.incrementGrade();
+    b4.decrementGrade();
+    b5.incrementGrade();
+
+    std::cout << b1 << std::endl;
+    std::cout << b2 << std::endl;
+    std::cout << b3 << std::endl;
+    std::cout << b4 << std::endl;
+    std::cout << b5 << std::endl;
+
+    return 0;
+}
