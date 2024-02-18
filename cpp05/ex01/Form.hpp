705f902d5e15854081b047649219526cf0c78447
Marwan Sekhsoukh <marwansekh123@gmail.com>
2024-02-18 21:48:38 +0100

    ex01

--- cpp05/ex01/Form.hpp
+++ cpp05/ex01/Form.hpp
@@ -6,7 +6,7 @@
 /*   By: msekhsou <msekhsou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/17 01:34:44 by msekhsou          #+#    #+#             */
-/*   Updated: 2024/02/18 21:48:33 by msekhsou         ###   ########.fr       */
+/*   Updated: 2024/02/18 21:48:37 by msekhsou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,7 @@ class Form
 		int const gradeToSign;
 		int const gradeToExecute;
 	public:
+
 	/*----------------------- Orthodox Canonical Form -----------------------*/
 		Form();
 		Form(const Form &copy);
