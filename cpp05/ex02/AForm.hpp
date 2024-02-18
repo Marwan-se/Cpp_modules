95a0a567af35d75badcb44ebdbbf0c5f7778d9e6
Marwan Sekhsoukh <marwansekh123@gmail.com>
2024-02-18 22:00:16 +0100

    ex02

---
+++ cpp05/ex02/AForm.hpp
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Aform.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msekhsou <msekhsou@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/18 22:00:06 by msekhsou          #+#    #+#             */
+/*   Updated: 2024/02/18 22:00:08 by msekhsou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FORM_HPP
+# define FORM_HPP
+
+# include <iostream>
+# include <string>
+# include "Bureaucrat.hpp"
+
+class Bureaucrat;
+class Form
+{
+	private:
+		std::string const name;
+		bool sign;
+		int const gradeToSign;
+		int const gradeToExecute;
+	public:
+
+	/*----------------------- Orthodox Canonical Form -----------------------*/
+		Form();
+		Form(const Form &copy);
+		Form(std::string const &name, int gradeToSign, int gradeToExecute);
+		Form &operator=(const Form &rhs);
+		~Form();
+	/*----------------------------------------------------------------------*/
+
+	/*----------------------------- Member Functions -----------------------*/
+		std::string getName() const;
+		bool		getSign() const;
+		int			getGradeToSign() const;
+		int			getGradeToExecute() const;
+		void		beSigned(Bureaucrat &bureaucrat);
+	/*----------------------------------------------------------------------*/
+
+	/*--------------------------- Nested Classes ---------------------------*/
+		class GradeTooHighException : public std::exception
+		{
+			public:
+				virtual const char *what() const throw();
+		};
+		class GradeTooLowException : public std::exception
+		{
+			public:
+				virtual const char *what() const throw();
+		};
+	/*----------------------------------------------------------------------*/
+};
+
+std::ostream &operator<<(std::ostream &out, const Form &form);
+
+#endif
