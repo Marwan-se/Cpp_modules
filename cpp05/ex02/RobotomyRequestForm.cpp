e6d2d7cef780b8f5b4640d544f9f3910e064f4c9
kurapikap@example.com <marwansekh123@gmail.com>
2024-02-21 17:19:59 +0000

    ex02

--- cpp05/ex02/RobotomyRequestForm.cpp
+++ cpp05/ex02/RobotomyRequestForm.cpp
@@ -2,30 +2,38 @@
 
 RobotomyRequestForm::RobotomyRequestForm() : AForm("RobotomyRequestForm", 72, 45)
 {
-    this->target = "default";
+	this->target = "default";
 }
 
 RobotomyRequestForm::RobotomyRequestForm(const RobotomyRequestForm &copy) : AForm(copy)
 {
-    *this = copy;
+	*this = copy;
 }
 
 RobotomyRequestForm::RobotomyRequestForm(std::string const &target) : AForm("RobotomyRequestForm", 72, 45)
 {
-    this->target = target;
+	this->target = target;
 }
 
 RobotomyRequestForm &RobotomyRequestForm::operator=(const RobotomyRequestForm &rhs)
 {
-    if (this != &rhs)
-        this->target = rhs.target;
-    return (*this);
+	if (this != &rhs)
+		this->target = rhs.target;
+	return (*this);
 }
 
 RobotomyRequestForm::~RobotomyRequestForm()
 {
 }
 
-void RobotomyRequestForm::execute(Bureaucrat const &executor) const
+void RobotomyRequestForm::execute(Bureaucrat const &exec) const
 {
+	AForm::execute(exec);
+	std::cout << "drilling noises..." << std::endl;
+	std::cout << "Vzzzzzzzzzzzzzz" << std::endl;
+	std::cout << "Vzzzzzzzzzz Vzzzzzzzzzz" << std::endl;
+	if (rand() % 2)
+		std::cout << this->target << " has been robotomized successfully." << std::endl;
+	else
+		std::cout << this->target << " robotomization failed." << std::endl;
 }
