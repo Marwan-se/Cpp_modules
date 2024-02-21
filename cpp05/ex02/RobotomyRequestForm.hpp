9d7a62ebdc64b21b5ea5dd91ac3105609f2701a0
kurapikap@example.com <marwansekh123@gmail.com>
2024-02-21 17:00:17 +0000

    ex02

--- cpp05/ex02/RobotomyRequestForm.hpp
+++ cpp05/ex02/RobotomyRequestForm.hpp
@@ -5,6 +5,35 @@
 # include <string>
 # include "AForm.hpp"
 
+class   RobotomyRequestForm : public AForm
+{
+	private:
+		std::string target;
+	public:
+	/*----------------------- Orthodox Canonical Form -----------------------*/
+		RobotomyRequestForm();
+		RobotomyRequestForm(const RobotomyRequestForm &copy);
+		RobotomyRequestForm(std::string const &target);
+		RobotomyRequestForm &operator=(const RobotomyRequestForm &rhs);
+		~RobotomyRequestForm();
+	/*----------------------------------------------------------------------*/
 
+	/*----------------------------- Member Functions -----------------------*/
+		void		execute(Bureaucrat const &executor) const;
+	/*----------------------------------------------------------------------*/
+
+	/*--------------------------- Nested Classes ---------------------------*/
+		class GradeTooHighException : public std::exception
+		{
+			public:
+				virtual const char *what() const throw();
+		};
+		class GradeTooLowException : public std::exception
+		{
+			public:
+				virtual const char *what() const throw();
+		};
+	/*----------------------------------------------------------------------*/
+};
 
 #endif
