6d51c95d35fe467981b3e5f9b36aebfdba61db98
kurapikap@example.com <marwansekh123@gmail.com>
2024-02-21 22:09:31 +0000

    ex02

---
+++ cpp05/ex02/PresidentialPardonForm.cpp
@@ -0,0 +1,33 @@
+#include "PresidentialPardonForm.hpp"
+
+PresidentialPardonForm::PresidentialPardonForm() : AForm("PresidentialPardonForm", 25, 5)
+{
+    this->target = "default";
+}
+
+PresidentialPardonForm::PresidentialPardonForm(const PresidentialPardonForm &copy) : AForm(copy)
+{
+    *this = copy;
+}
+
+PresidentialPardonForm::PresidentialPardonForm(std::string const &target) : AForm("PresidentialPardonForm", 25, 5)
+{
+    this->target = target;
+}
+
+PresidentialPardonForm &PresidentialPardonForm::operator=(const PresidentialPardonForm &rhs)
+{
+    if (this != &rhs)
+        this->target = rhs.target;
+    return (*this);
+}
+
+PresidentialPardonForm::~PresidentialPardonForm()
+{
+}
+
+void	PresidentialPardonForm::execute(Bureaucrat const &exec) const
+{
+    AForm::execute(exec);
+    std::cout << "this " << this->target << " has been pardoned by Zafod Beeblebrox." << std::endl;
+}
