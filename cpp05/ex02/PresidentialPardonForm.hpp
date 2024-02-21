2fa3f45a5fe75bcdef11c9423b4e37ab378d6d6e
kurapikap@example.com <marwansekh123@gmail.com>
2024-02-21 17:20:48 +0000

    ex02

--- cpp05/ex02/PresidentialPardonForm.hpp
+++ cpp05/ex02/PresidentialPardonForm.hpp
@@ -1,1 +1,39 @@
-#include
+#ifndef PRESIDENTIALPARDONFORM_HPP
+# define PRESIDENTIALPARDONFORM_HPP
+
+# include <iostream>
+# include <string>
+# include "AForm.hpp"
+
+class  PresidentialPardonForm : public AForm
+{
+    private:
+        std::string target;
+    public:
+    /*----------------------- Orthodox Canonical Form -----------------------*/
+        PresidentialPardonForm();
+        PresidentialPardonForm(const PresidentialPardonForm &copy);
+        PresidentialPardonForm(std::string const &target);
+        PresidentialPardonForm &operator=(const PresidentialPardonForm &rhs);
+        ~PresidentialPardonForm();
+    /*----------------------------------------------------------------------*/
+
+    /*----------------------------- Member Functions -----------------------*/
+        void		execute(Bureaucrat const &executor) const;
+    /*----------------------------------------------------------------------*/
+
+    /*--------------------------- Nested Classes ---------------------------*/
+        class GradeTooHighException : public std::exception
+        {
+            public:
+                virtual const char *what() const throw();
+        };
+        class GradeTooLowException : public std::exception
+        {
+            public:
+                virtual const char *what() const throw();
+        };
+    /*----------------------------------------------------------------------*/
+};
+
+#endif
