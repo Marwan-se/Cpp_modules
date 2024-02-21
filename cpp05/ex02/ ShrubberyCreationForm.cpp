2c8bf49e4d802d9e0b39ea0c4be267ee146ebd24
kurapikap@example.com <marwansekh123@gmail.com>
2024-02-21 17:09:00 +0000

    ex02

---
+++ cpp05/ex02/ ShrubberyCreationForm.cpp
@@ -0,0 +1,51 @@
+#include "ShrubberyCreationForm.hpp"
+
+ShrubberyCreationForm::ShrubberyCreationForm() : AForm("ShrubberyCreationForm", 145, 137)
+{
+	this->target = "default";
+}
+
+ShrubberyCreationForm::ShrubberyCreationForm(const ShrubberyCreationForm &copy) : AForm(copy)
+{
+	*this = copy;
+}
+
+ShrubberyCreationForm::ShrubberyCreationForm(std::string const &target) : AForm("ShrubberyCreationForm", 145, 137)
+{
+	this->target = target;
+}
+
+ShrubberyCreationForm &ShrubberyCreationForm::operator=(const ShrubberyCreationForm &rhs)
+{
+	if (this != &rhs)
+		this->target = rhs.target;
+	return (*this);
+}
+
+ShrubberyCreationForm::~ShrubberyCreationForm()
+{
+}
+
+void	ShrubberyCreationForm::execute(Bureaucrat const &exec) const
+{
+	AForm::execute(exec);
+	std::ofstream file;
+	file.open(this->target + "_shrubbery");
+	std::cout << "            #### ####" << std::endl;
+    std::cout << "        ### \\/#|### |/####" << std::endl;
+    std::cout << "       ##\\/#/ \\||/##/_/##/_#" << std::endl;
+    std::cout << "     ###  \\/###|/ \\/ # ###" << std::endl;
+    std::cout << "   ##_\\_#\\_\\## | #/###_/_####" << std::endl;
+    std::cout << "  ## #### # \\ #| /  #### ##/##" << std::endl;
+    std::cout << "   __#_--###`  |{,###---###-~" << std::endl;
+    std::cout << "             \\ }{" << std::endl;
+    std::cout << "              }}{" << std::endl;
+    std::cout << "              }}{" << std::endl;
+    std::cout << "         ejm  {{}" << std::endl;
+    std::cout << "        , -=-~{ .-^- _" << std::endl;
+    std::cout << "              `}" << std::endl;
+    std::cout << "               {" << std::endl;
+	std::endl;
+	std::cout << "File " << this->target << "_shrubbery created." << std::endl;
+	file.close();
+}
