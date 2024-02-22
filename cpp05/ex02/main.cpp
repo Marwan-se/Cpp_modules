e5363b31496198a0614e8b6c9d23068bac6ff52e
Marwan Sekhsoukh <marwansekh123@gmail.com>
2024-02-22 18:18:51 +0100

    ex02

--- cpp05/ex02/main.cpp
+++ cpp05/ex02/main.cpp
@@ -6,7 +6,17 @@
 /*   By: msekhsou <msekhsou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/22 17:36:43 by msekhsou          #+#    #+#             */
-/*   Updated: 2024/02/22 17:36:44 by msekhsou         ###   ########.fr       */
+/*   Updated: 2024/02/22 18:17:55 by msekhsou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include " ShrubberyCreationForm.hpp"
+#include "RobotomyRequestForm.hpp"
+#include "PresidentialPardonForm.hpp"
+#include "Bureaucrat.hpp"
+#include "AForm.hpp"
+
+int main()
+{
+    
+}
