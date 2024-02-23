fb2ad102f88cb690f07f3dc577bd62fb5bb668fe
kurapikap@example.com <marwansekh123@gmail.com>
2024-02-23 22:56:10 +0000

    ex03

--- cpp05/ex03/main.cpp
+++ cpp05/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: msekhsou <msekhsou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/22 17:36:43 by msekhsou          #+#    #+#             */
-/*   Updated: 2024/02/23 01:59:33 by msekhsou         ###   ########.fr       */
+/*   Updated: 2024/02/23 22:56:09 by msekhsou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,26 +18,7 @@
 
 int main()
 {
-    Bureaucrat bureaucrat("pp", 10);
 
-    try
-    {
-        ShrubberyCreationForm shrubberyForm("ohio");
-        RobotomyRequestForm robotomyForm("daft");
-        PresidentialPardonForm pardonForm("fromage");
-
-        bureaucrat.signForm(shrubberyForm);
-        bureaucrat.signForm(robotomyForm);
-        bureaucrat.signForm(pardonForm);
-
-        shrubberyForm.execute(bureaucrat);
-        robotomyForm.execute(bureaucrat);
-        pardonForm.execute(bureaucrat);
-    }
-    catch (const std::exception& e)
-    {
-        std::cout << "Exception caught: " << e.what() << std::endl;
-    }
 
     return 0;
 }
