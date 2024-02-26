eb33a7ae353f3f427fbba81774ac60b10d139dd2
Marwan Sekhsoukh <marwansekh123@gmail.com>
2024-02-26 02:50:51 +0100

    ex00

--- cpp06/ex00/ScalarConverter.cpp
+++ cpp06/ex00/ScalarConverter.cpp
@@ -6,9 +6,30 @@
 /*   By: msekhsou <msekhsou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/26 02:50:07 by msekhsou          #+#    #+#             */
-/*   Updated: 2024/02/26 02:50:19 by msekhsou         ###   ########.fr       */
+/*   Updated: 2024/02/26 02:50:50 by msekhsou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScalarConverter.hpp"
 
+/*-------------------------- Constructors -------------------------*/
+
+ScalarConverter::ScalarConverter()
+{
+}
+
+ScalarConverter::ScalarConverter(const ScalarConverter &copy)
+{
+    *this = copy;
+}
+
+ScalarConverter::~ScalarConverter()
+{
+}
+
+ScalarConverter &ScalarConverter::operator=(const ScalarConverter &rhs)
+{
+    if (this != &rhs)
+        *this = rhs;
+    return *this;
+}
