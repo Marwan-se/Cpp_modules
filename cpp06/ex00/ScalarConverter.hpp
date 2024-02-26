56bf98a1f151b04975d0476980ca3789709a1628
Marwan Sekhsoukh <marwansekh123@gmail.com>
2024-02-26 02:43:53 +0100

    ex00

--- cpp06/ex00/ScalarConverter.hpp
+++ cpp06/ex00/ScalarConverter.hpp
@@ -6,7 +6,8 @@
 /*   By: msekhsou <msekhsou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/26 02:43:37 by msekhsou          #+#    #+#             */
-/*   Updated: 2024/02/26 02:43:38 by msekhsou         ###   ########.fr       */
+/*   Updated: 2024/02/26 02:43:48 by msekhsou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef SCALARCONVERTER_HPP
