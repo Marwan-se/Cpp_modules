7be1e9365345b6ab1fb5adef2fe3fbff1a3e7a26
Marwan Sekhsoukh <marwansekh123@gmail.com>
2024-02-26 02:49:59 +0100

    ex00

--- cpp06/ex00/ScalarConverter.hpp
+++ cpp06/ex00/ScalarConverter.hpp
@@ -6,11 +6,34 @@
 /*   By: msekhsou <msekhsou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/26 02:43:37 by msekhsou          #+#    #+#             */
-/*   Updated: 2024/02/26 02:44:09 by msekhsou         ###   ########.fr       */
+/*   Updated: 2024/02/26 02:49:39 by msekhsou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SCALARCONVERTER_HPP
 # define SCALARCONVERTER_HPP
+
+#include <string>
+
+class	ScalarConverter
+{
+	public:
+
+	/*--------------------- Orthodox Canonical Form ----------------------*/
+		ScalarConverter();
+		ScalarConverter(const ScalarConverter &copy);
+		~ScalarConverter();
+		ScalarConverter &operator=(const ScalarConverter &rhs);
+	/*---------------------------------------------------------------------*/
+
+	/*-------------------------- Member Functions -------------------------*/
+		void	convert(std::string str);
+		void	printChar();
+		void	printInt();
+		void	printFloat();
+		void	printDouble();
+	/*---------------------------------------------------------------------*/
+		
+};
 
 #endif
