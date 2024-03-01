901fe95048505498878ec0354dbb2b9e5f4b9706
Marwan Sekhsoukh <marwansekh123@gmail.com>
2024-03-01 20:28:17 +0100

    ex01

--- cpp06/ex01/main.cpp
+++ cpp06/ex01/main.cpp
@@ -6,24 +6,28 @@
 /*   By: msekhsou <msekhsou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/27 02:36:48 by msekhsou          #+#    #+#             */
-/*   Updated: 2024/03/01 10:22:38 by msekhsou         ###   ########.fr       */
+/*   Updated: 2024/03/01 16:32:45 by msekhsou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Serializer.hpp"
 
+
 int main()
 {
-    Data    data;
-    data.value = 42;
-    data.str = "Hello World!";
+    Data myData;
+    myData.value = 42;
+
+    uintptr_t serializedValue = Serializer::serialize(&myData);
+
+    std::cout << "Serialized Value : " << &serializedValue << std::endl;
+
+    Data* deserializedData = Serializer::deserialize(serializedValue);
+
+    std::cout << "Serialization and deserialization successful!" << std::endl;
+    std::cout << "Original value: " << myData.value << std::endl;
+    std::cout << "Deserialized value: " << deserializedData->value << std::endl;
     
-    uintptr_t serialize = Serializer::serialize(&data);
-
-    Data *deserialize = Serializer::deserialize(serialize);
-
-    std::cout << "Value: " << deserialize->value << std::endl;
-    std::cout << "String: " << deserialize->str << std::endl;
-
-
+    return 0;
 }
+
