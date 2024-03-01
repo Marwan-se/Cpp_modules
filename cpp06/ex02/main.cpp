6652f72ca8a41c13ca00be63a53789217f4b244f
Marwan Sekhsoukh <marwansekh123@gmail.com>
2024-03-01 13:39:35 +0100

    ex02

---
+++ cpp06/ex02/main.cpp
@@ -0,0 +1,90 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msekhsou <msekhsou@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/01 11:01:07 by msekhsou          #+#    #+#             */
+/*   Updated: 2024/03/01 13:39:30 by msekhsou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Base.hpp"
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
+#include <cstdlib>
+#include <iostream>
+#include <stdexcept>
+
+
+Base *generate(void)
+{
+	srand(time(0));
+	int random = rand() % 3;
+	switch (random)
+	{
+		case 0:
+			return (new A());
+		case 1:
+			return (new B());
+		case 2:
+			return (new C());
+		default:
+			throw std::runtime_error("failed to generate an instance");
+	}
+}
+
+void    indentify(Base *p)
+{
+	if (dynamic_cast<A*>(p))
+		std::cout << "A" << std::endl;
+	else if (dynamic_cast<B*>(p))
+		std::cout << "B" << std::endl;
+	else if (dynamic_cast<C*>(p))
+		std::cout << "C" << std::endl;
+	else
+		std::cout << "Unknown" << std::endl;
+}
+
+void    indentify(Base &p)
+{
+	try
+	{
+		A &a = dynamic_cast<A&>(p);
+		(void)a;
+		std::cout << "A" << std::endl;
+	}
+	catch (std::bad_cast &e)
+	{
+		try
+		{
+			B &b = dynamic_cast<B&>(p);
+			(void)b;
+			std::cout << "B" << std::endl;
+		}
+		catch (std::bad_cast &e)
+		{
+			try
+			{
+				C &c = dynamic_cast<C&>(p);
+				(void)c;
+				std::cout << "C" << std::endl;
+			}
+			catch (std::bad_cast &e)
+			{
+				std::cout << "Unknown" << std::endl;
+			}
+		}
+	}
+}
+
+int main()
+{
+	Base *base = generate();
+	indentify(base);
+	indentify(*base);
+	delete base;
+	return (0);
+}
