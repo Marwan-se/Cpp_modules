877e1ecce21a46af00ede6533733843836c98225
Marwan Sekhsoukh <marwansekh123@gmail.com>
2024-03-02 23:35:42 +0100

    ex01

--- cpp06/ex01/main.cpp
+++ cpp06/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: msekhsou <msekhsou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/27 02:36:48 by msekhsou          #+#    #+#             */
-/*   Updated: 2024/03/01 16:32:45 by msekhsou         ###   ########.fr       */
+/*   Updated: 2024/03/02 23:29:07 by msekhsou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,19 @@ int main()
 {
     Data myData;
     myData.value = 42;
+    myData.str = "marwan";
 
     uintptr_t serializedValue = Serializer::serialize(&myData);
 
-    std::cout << "Serialized Value : " << &serializedValue << std::endl;
+    std::cout << "Serialized Value : " << serializedValue << std::endl;
 
     Data* deserializedData = Serializer::deserialize(serializedValue);
 
     std::cout << "Serialization and deserialization successful!" << std::endl;
     std::cout << "Original value: " << myData.value << std::endl;
     std::cout << "Deserialized value: " << deserializedData->value << std::endl;
+    std::cout << "Original string: " << myData.str << std::endl;
+    std::cout << "Deserialized string: " << deserializedData->str << std::endl;
     
     return 0;
 }
