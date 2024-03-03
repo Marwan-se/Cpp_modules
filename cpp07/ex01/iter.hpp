4ce4ae0ea784029e6f8f60e9570febb05d7a5c8d
Marwan Sekhsoukh <marwansekh123@gmail.com>
2024-03-03 22:45:54 +0100

    ex01

--- cpp07/ex01/iter.hpp
+++ cpp07/ex01/iter.hpp
@@ -6,7 +6,23 @@
 /*   By: msekhsou <msekhsou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/03 22:02:14 by msekhsou          #+#    #+#             */
-/*   Updated: 2024/03/03 22:02:15 by msekhsou         ###   ########.fr       */
+/*   Updated: 2024/03/03 22:37:46 by msekhsou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef ITER_HPP
+#define ITER_HPP
+
+#include "iostream"
+#include <cstddef>
+#include <string>
+
+template <class T1, class T2> void  iter(T1 &array, size_t arrlen, T2 func)
+{
+    for (size_t i = 0; i < arrlen; i++)
+    {
+        func(array[i]);
+    }
+}
+
+#endif
--- cpp07/ex01/main.cpp
+++ cpp07/ex01/main.cpp
@@ -6,7 +6,40 @@
 /*   By: msekhsou <msekhsou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/03 22:02:16 by msekhsou          #+#    #+#             */
-/*   Updated: 2024/03/03 22:02:17 by msekhsou         ###   ########.fr       */
+/*   Updated: 2024/03/03 22:45:39 by msekhsou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "iter.hpp"
+
+class Awesome
+{
+  public:
+    Awesome( void ) : _n( 42 ) { return; }
+    int get( void ) const { return this->_n; }
+  private:
+    int _n;
+};  
+
+std::ostream & operator<<( std::ostream & o, Awesome const & rhs )
+{
+  o << rhs.get();
+  return o;
+}
+
+template< typename T >
+void print( T& x )
+{
+  std::cout << x << std::endl;
+  return;
+}
+
+int main() {
+  int tab[] = { 0, 1, 2, 3, 4 };
+  Awesome tab2[5];
+
+  iter( tab, 5, print<const int> );
+  iter( tab2, 5, print<Awesome> );
+
+  return 0;
+}
