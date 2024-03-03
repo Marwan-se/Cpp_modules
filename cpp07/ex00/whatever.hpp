a2199ecec2751aabf56a344c8ca0f8f868e4a28b
Marwan Sekhsoukh <marwansekh123@gmail.com>
2024-03-03 21:55:18 +0100

    ex00

--- cpp07/ex00/whatever.hpp
+++ cpp07/ex00/whatever.hpp
@@ -6,7 +6,7 @@
 /*   By: msekhsou <msekhsou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/03 17:34:23 by msekhsou          #+#    #+#             */
-/*   Updated: 2024/03/03 21:35:59 by msekhsou         ###   ########.fr       */
+/*   Updated: 2024/03/03 21:49:10 by msekhsou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,11 @@
 #include <string>
 #include <iostream>
 
-template <class T>  void   swap(T x, T y)
+template <class T>  void   swap(T& a, T& b)
 {
-    T tmp;
-    tmp = x;
-    x = y;
-    y = tmp;
+    T tmp = a;
+    a = b;
+    b = tmp;
 }
 
 template <class T1> T1 min(T1 a, T1 b)
