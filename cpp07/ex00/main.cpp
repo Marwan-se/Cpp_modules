0717578a35557bbc103694d2133d4cbb30c9e5ee
Marwan Sekhsoukh <marwansekh123@gmail.com>
2024-03-03 21:37:07 +0100

    ex00

--- cpp07/ex00/main.cpp
+++ cpp07/ex00/main.cpp
@@ -6,7 +6,29 @@
 /*   By: msekhsou <msekhsou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/03 17:34:32 by msekhsou          #+#    #+#             */
-/*   Updated: 2024/03/03 17:34:33 by msekhsou         ###   ########.fr       */
+/*   Updated: 2024/03/03 21:37:04 by msekhsou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "whatever.hpp"
+#include <iostream>
+
+int main( void )
+{
+    int a = 2;
+    int b = 3;
+
+    ::swap( a, b );
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+    std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+    std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+
+    std::string c = "chaine1";
+    std::string d = "chaine2";
+
+    ::swap(c, d);
+    std::cout << "c = " << c << ", d = " << d << std::endl;
+    std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+    std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+    return 0;
+}
--- cpp07/ex00/whatever.hpp
+++ cpp07/ex00/whatever.hpp
@@ -6,7 +6,36 @@
 /*   By: msekhsou <msekhsou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/03 17:34:23 by msekhsou          #+#    #+#             */
-/*   Updated: 2024/03/03 17:34:24 by msekhsou         ###   ########.fr       */
+/*   Updated: 2024/03/03 21:35:59 by msekhsou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef WHATEVER_HPP
+#define WHATEVER_HPP
+
+#include <string>
+#include <iostream>
+
+template <class T>  void   swap(T x, T y)
+{
+    T tmp;
+    tmp = x;
+    x = y;
+    y = tmp;
+}
+
+template <class T1> T1 min(T1 a, T1 b)
+{
+    if (a < b)
+        return(a);
+    return(b);
+}
+
+template<class T2>  T2 max(T2 a, T2 b)
+{
+    if (a > b)
+        return(a);
+    return(b);
+}
+
+#endif
