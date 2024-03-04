b475ee4d30abf7496714be1b56621350b5133d38
Marwan Sekhsoukh <marwansekh123@gmail.com>
2024-03-04 14:34:01 +0100

    ex02

---
+++ cpp07/ex02/Array.hpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Array.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msekhsou <msekhsou@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/03 23:37:44 by msekhsou          #+#    #+#             */
+/*   Updated: 2024/03/04 14:30:32 by msekhsou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ARRAY_HPP
+#define ARRAY_HPP
+
+#include <iostream>
+
+template<class T>   class Array
+{
+	Array() :arr(nullptr), len(0){};
+};
+
+#endif
