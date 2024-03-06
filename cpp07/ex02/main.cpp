8d34f32c07ee8ef93ddf2c55d3d790a3fc12a2d2
Marwan Sekhsoukh <marwansekh123@gmail.com>
2024-03-06 23:56:01 +0100

    ex02

--- cpp07/ex02/main.cpp
+++ cpp07/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: msekhsou <msekhsou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/03 23:37:46 by msekhsou          #+#    #+#             */
-/*   Updated: 2024/03/04 19:59:44 by msekhsou         ###   ########.fr       */
+/*   Updated: 2024/03/06 23:50:52 by msekhsou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,80 +16,80 @@
 #define MAX_VAL 750
 
 
-// int main(int, char**)
-// {
-//     Array<int> numbers(MAX_VAL);
-//     int *mirror = new int[MAX_VAL];
-//     srand(time(NULL));
-//     for (int i = 0; i < MAX_VAL; i++)
-//     {
-//         const int value = rand();
-//         numbers[i] = value;
-//         mirror[i] = value;
-//     }
-//     //SCOPE
-//     {
-//         Array<int> tmp = numbers;
-//         Array<int> test(tmp);
-//     }
-
-//     for (int i = 0; i < MAX_VAL; i++)
-//     {
-//         if (mirror[i] != numbers[i])
-//         {
-//             std::cerr << "didn't save the same value!!" << std::endl;
-//             return 1;
-//         }
-//     }
-//     try
-//     {
-//         numbers[-2] = 0;
-//     }
-//     catch(const std::exception& e)
-//     {
-//         std::cerr << e.what() << '\n';
-//     }
-//     try
-//     {
-//         numbers[MAX_VAL] = 0;
-//     }
-//     catch(const std::exception& e)
-//     {
-//         std::cerr << e.what() << '\n';
-//     }
-
-//     for (int i = 0; i < MAX_VAL; i++)
-//     {
-//         numbers[i] = rand();
-//     }
-//     delete [] mirror;//
-//     return 0;
-// }
-
-
-int main()
+int main(int, char**)
 {
-    int size = 5;
-    int* myArray = new int[size];
+    Array<int> numbers(MAX_VAL);
+    int *mirror = new int[MAX_VAL];
+    srand(time(NULL));
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        const int value = rand();
+        numbers[i] = value;
+        mirror[i] = value;
+    }
+    //SCOPE
+    {
+        Array<int> tmp = numbers;
+        Array<int> test(tmp);
+    }
 
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        if (mirror[i] != numbers[i])
+        {
+            std::cerr << "didn't save the same value!!" << std::endl;
+            return 1;
+        }
+    }
     try
     {
-        for (int i = 0; i < size; ++i)
-        {
-            myArray[i] = i * 10;
-        }
-
-        int outOfBoundsIndex = size + 1;
-        int element = myArray[outOfBoundsIndex];
-
-        std::cout << "Element at index " << outOfBoundsIndex << ": " << element << std::endl;
+        numbers[-2] = 0;
     }
-    catch (const std::exception& e)
+    catch(const std::exception& e)
     {
-        std::cerr << "Exception: " << e.what() << std::endl;
+        std::cerr << e.what() << '\n';
+    }
+    try
+    {
+        numbers[MAX_VAL] = 0;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
     }
 
-    delete[] myArray;
-
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        numbers[i] = rand();
+    }
+    delete [] mirror;//
     return 0;
 }
+
+
+// int main()
+// {
+//     int size = 5;
+//     int* myArray = new int[size];
+
+//     try
+//     {
+//         for (int i = 0; i < size; ++i)
+//         {
+//             myArray[i] = i * 10;
+//         }
+
+//         int outOfBoundsIndex = size + 1;
+//         int element = myArray[outOfBoundsIndex];
+
+//         std::cout << "Element at index " << outOfBoundsIndex << ": " << element << std::endl;
+//     }
+//     catch (const std::exception& e)
+//     {
+//         std::cerr << "Exception: " << e.what() << std::endl;
+//     }
+
+//     delete[] myArray;
+
+//     return 0;
+// }
