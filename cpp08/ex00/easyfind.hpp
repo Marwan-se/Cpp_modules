cb7a50ce7d44ef394fa6952e96fe5af9f6f4585f
Marwan Sekhsoukh <marwansekh123@gmail.com>
2024-03-06 17:14:39 +0100

    ex00

--- cpp08/ex00/easyfind.hpp
+++ cpp08/ex00/easyfind.hpp
@@ -6,7 +6,22 @@
 /*   By: msekhsou <msekhsou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/05 17:49:25 by msekhsou          #+#    #+#             */
-/*   Updated: 2024/03/05 17:49:26 by msekhsou         ###   ########.fr       */
+/*   Updated: 2024/03/06 17:12:30 by msekhsou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef EASYFIND_HPP
+#define EASYFIND_HPP
+
+#include <iostream>
+#include <string>
+#include <iterator>
+#include <vector>
+#include <algorithm>
+
+
+//Section C.13.5 
+
+
+
+#endif
