ee62a317497f7afde3ba0c1c0875271203abcb69
Marwan Sekhsoukh <marwansekh123@gmail.com>
2024-03-06 21:15:12 +0100

    ex01

--- cpp08/ex01/Span.cpp
+++ cpp08/ex01/Span.cpp
@@ -6,7 +6,34 @@
 /*   By: msekhsou <msekhsou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/06 18:24:53 by msekhsou          #+#    #+#             */
-/*   Updated: 2024/03/06 18:24:54 by msekhsou         ###   ########.fr       */
+/*   Updated: 2024/03/06 21:12:48 by msekhsou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "Span.hpp"
+
+Span::Span()
+{
+}
+
+Span::~Span()
+{
+}
+
+Span::Span(const unsigned int n)
+{
+    N = n;
+}
+
+Span::Span(const Span &s)
+{
+    *this = s;
+}
+
+Span &Span::operator=(const Span &rhs)
+{
+    N = rhs.N;
+    v = rhs.v;
+    return *this;
+}
 
--- cpp08/ex01/Span.hpp
+++ cpp08/ex01/Span.hpp
@@ -6,13 +6,31 @@
 /*   By: msekhsou <msekhsou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/06 18:25:00 by msekhsou          #+#    #+#             */
-/*   Updated: 2024/03/06 18:25:22 by msekhsou         ###   ########.fr       */
+/*   Updated: 2024/03/06 21:10:46 by msekhsou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SPAN_HPP
 #define SPAN_HPP
 
+#include <iostream>
+#include <vector>
+#include <algorithm>
 
+class Span
+{
+	private:
+		unsigned int N;
+		std::vector<int> v;
+	public:
+		Span();
+		Span(const unsigned int n);
+		Span(const Span &s);
+		~Span();
+		Span &operator=(const Span &s);
+		void addNumber(unsigned int n);
+		void	shortestSpan();
+		void	longestSpan();
+};
 
 #endif
