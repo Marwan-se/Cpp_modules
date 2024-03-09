53db9a87181eb4e3ccada61f1adfbc1ed3b75583
Marwan Sekhsoukh <marwansekh123@gmail.com>
2024-03-09 21:07:46 +0100

    ex00

--- cpp09/ex00/main.cpp
+++ cpp09/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: msekhsou <msekhsou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/09 10:33:20 by msekhsou          #+#    #+#             */
-/*   Updated: 2024/03/09 12:56:22 by msekhsou         ###   ########.fr       */
+/*   Updated: 2024/03/09 20:59:24 by msekhsou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,7 @@ void    valid_args()
 void    valid_file()
 {
     std::cerr << "Error: could not open file." << std::endl;
+    exit(1);
 }
 
 int main(int ac, char **av)
