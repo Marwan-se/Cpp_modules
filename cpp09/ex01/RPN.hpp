a5fe77881030f4969247f2e0a3ba58e1ca31a36f
Marwan Sekhsoukh <marwansekh123@gmail.com>
2024-03-10 02:09:46 +0000

    CPP08

---
+++ cpp09/ex01/RPN.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   RPN.hpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msekhsou <msekhsou@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/10 01:36:19 by msekhsou          #+#    #+#             */
+/*   Updated: 2024/03/10 02:09:37 by msekhsou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef RPN_HPP
+#define RPN_HPP
+
+#include <iostream>
+#include <stack>
+#include <string>
+
+class RPN
+{
+	public:
+		RPN();
+		RPN(const RPN &copy);
+		~RPN();
+		RPN &operator=(const RPN &copy);
+		
+		
+};
+
+#endif
