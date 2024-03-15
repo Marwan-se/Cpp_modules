6d14225c7fe6f650c69bff947d647884aa2ad8aa
kurapikap@example.com <marwansekh123@gmail.com>
2024-03-15 04:07:23 +0000

    ex02

--- cpp09/ex02/PmergeMe.cpp
+++ cpp09/ex02/PmergeMe.cpp
@@ -6,7 +6,33 @@
 /*   By: msekhsou <msekhsou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/13 03:43:35 by msekhsou          #+#    #+#             */
-/*   Updated: 2024/03/13 03:43:36 by msekhsou         ###   ########.fr       */
+/*   Updated: 2024/03/14 18:24:14 by msekhsou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "PmergeMe.hpp"
+
+std::vector<int> vec;
+std::deque<int> deq;
+
+PmergeMe::PmergeMe()
+{
+}
+
+PmergeMe::PmergeMe(const PmergeMe &copy)
+{
+	*this = copy;
+}
+
+PmergeMe::~PmergeMe()
+{
+}
+
+PmergeMe &PmergeMe::operator=(const PmergeMe &copy)
+{
+	(void)copy;
+	return (*this);
+}
+
+/*-----------------------------MERGE FUNCTIONS---------------------------------*/
 
