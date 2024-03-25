777ae481e7c4e5d36a427f0f700de49796457557
Marwan Sekhsoukh <marwansekh123@gmail.com>
2024-03-25 02:59:17 +0000

    ex02

--- cpp09/ex02/PmergeMe.cpp
+++ cpp09/ex02/PmergeMe.cpp
@@ -6,19 +6,21 @@
 /*   By: msekhsou <msekhsou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/13 03:43:35 by msekhsou          #+#    #+#             */
-/*   Updated: 2024/03/25 02:00:43 by msekhsou         ###   ########.fr       */
+/*   Updated: 2024/03/25 02:49:14 by msekhsou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PmergeMe.hpp"
+#include <cstddef>
 
 
-void	merge()
+// void	merge()
 
-void	mergesort(std::vectot<int> &vec)
-{
+// void	mergesort(std::vectot<int> &vec)
+// {
 	
-}
+// }
+
 std::vector<std::pair<int, int> > MakePairs(std::vector<int> &vec)
 {
 	std::vector<std::pair<int, int> > pairs;
@@ -27,7 +29,12 @@ std::vector<std::pair<int, int> > MakePairs(std::vector<int> &vec)
 	for (size_t i = 0; i < size; i += 2)
 	{
 		if (i + 1 < size)
-			pairs.push_back(std::make_pair(vec[i], vec[i + 1]));
+		{
+			if (vec[i] > vec[i + 1])
+				pairs.push_back(std::make_pair(vec[i + 1], vec[i]));
+			else
+				pairs.push_back(std::make_pair(vec[i], vec[i + 1]));
+		}
 	}
 	return pairs;
 }
@@ -44,10 +51,19 @@ bool catchStruggler(int &straggler, std::vector<int> &arr)
     return false;
 }
 
+void	create_x_y(std::vector<std::pair<int, int> > &pairs, std::vector<int> &x, std::vector<int> &y)
+{
+	for (size_t i = 0; i < pairs.size(); i++)
+	{
+		x.push_back(pairs[i].first);
+		y.push_back(pairs[i].second);
+	}
+}
+
 void	algorithm(std::vector<int> &vec)
 {
 	std::vector<std::pair<int, int> > pairs;
-	int straggler = 0;
+	int straggler = -1;
 	
 	catchStruggler(straggler, vec);
 
@@ -56,5 +72,7 @@ void	algorithm(std::vector<int> &vec)
 	for (size_t i = 0; i < pairs.size(); ++i)
     	std::cout << "<" << pairs[i].first << ", " << pairs[i].second << "> ";
 
+	std::vector<int> x, y;
+	create_x_y(pairs, x, y);
 
 }
