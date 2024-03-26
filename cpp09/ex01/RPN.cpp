3de9a6adaf22919a10f9844392fce10f1137cdab
Marwan Sekhsoukh <marwansekh123@gmail.com>
2024-03-26 02:28:42 +0000

    ex01

--- cpp09/ex01/RPN.cpp
+++ cpp09/ex01/RPN.cpp
@@ -6,7 +6,7 @@
 /*   By: msekhsou <msekhsou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/10 01:36:23 by msekhsou          #+#    #+#             */
-/*   Updated: 2024/03/12 01:04:48 by msekhsou         ###   ########.fr       */
+/*   Updated: 2024/03/22 03:30:00 by msekhsou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,13 +43,19 @@ int	evaluate(int a, int b, char op)
 	else if (op == '*')
 		return (a * b);
 	else if (op == '/')
+	{
+		if (b == 0)
+		{
+			throw std::invalid_argument("Division by zero");
+		}
 		return (a / b);
+	}
 	return (0);
 }
 
 void	RPN::read_input(std::string input)
 {
-	size_t operands = 0;
+	int operands = 0;
 	std::string ops = "+-*/";
 
 	for (size_t i = 0; i < input.length(); i++)
@@ -60,8 +66,7 @@ void	RPN::read_input(std::string input)
 			int num = atoi(&input[i]);
 			if (num > 9)
 			{
-				std::cout << "Invalid input" << std::endl;
-				exit(1);
+				throw std::invalid_argument("Invalid input");
 			}
 			_myStack.push(num);
 		}
@@ -69,8 +74,7 @@ void	RPN::read_input(std::string input)
 		{
 			if (operands < 2)
 			{
-				std::cout << "Not enough operands" << std::endl;
-				exit(1);
+				throw std::invalid_argument("Invalid input");
 			}
 			operands--;
 			int n1 = _myStack.top();
@@ -80,5 +84,9 @@ void	RPN::read_input(std::string input)
 			_myStack.push(evaluate(n2, n1, input[i]));
 		}
 	}
+	if (_myStack.size() != 1)
+	{
+		throw std::invalid_argument("Invalid input");
+	} 
 	std::cout << _myStack.top() << std::endl;
 }
--- cpp09/ex01/main.cpp
+++ cpp09/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: msekhsou <msekhsou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/10 01:36:26 by msekhsou          #+#    #+#             */
-/*   Updated: 2024/03/11 21:22:54 by msekhsou         ###   ########.fr       */
+/*   Updated: 2024/03/21 22:13:03 by msekhsou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,15 @@ int main(int ac, char **av)
 		std::cout << "Usage: ./RPN \"[RPN expression]\"" << std::endl;
 		return (1);
 	}
-	
 	std::string input = av[1];
-	rpn.read_input(input);
-	
+
+	try
+	{
+		rpn.read_input(input);
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
 	return 0;
 }
